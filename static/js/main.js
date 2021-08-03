160faafbbf20dac3079cc273c98bf3cc84bb7e70
Yue Zhao <2499059z@student.gla.ac.uk>
2021-08-03 12:48:48 +0100

    update category page

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,15 @@
 function click_add_comment_btn() {
     console.log(123)
 }
+
+function hover_page_list(){
+
+}
 $(function(){
     
     $("#add_comment_btn").click(function(){
         console.log('sadsadass');
       });
-  
+    
  });
 
