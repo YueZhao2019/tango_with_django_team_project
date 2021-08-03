f20a111a9984a54641e832117916e84ee4f7d1e5
Yue Zhao <2499059z@student.gla.ac.uk>
2021-08-03 10:13:44 +0100

    import main.js

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+function click_add_comment_btn() {
+    console.log(123)
+}
+$(function(){
+    
+    $("#add_comment_btn").click(function(){
+        console.log('sadsadass');
+      });
+  
+ });
+
