72ac65cf8c5ab77a90b835852884aa4b01526967
2598798g@student.gla.ac.uk <2598798g@student.gla.ac.uk>
2021-08-04 12:56:13 +0800

    unit testing

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from rango.models import Category
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTests(TestCase):
+    def test_index_view_with_no_categories(self):
+        """
+        If no categories exist, the appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('rango:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no categories present.')
+        self.assertQuerysetEqual(response.context['categories'], [])
+
+
+
