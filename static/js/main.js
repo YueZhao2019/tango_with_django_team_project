3d6033f3df87f8157c8f40adb7120b3fe7a53935
KaiyinisDad <88020704+KaiyinisDad@users.noreply.github.com>
2021-08-04 23:29:24 +0800

    make a popuo to make sure you won't delete by accident

--- static/js/main.js
+++ static/js/main.js
@@ -17,6 +17,22 @@ function delete_comment(comment_id) {
 
 
 }
+ // 确认删除评论的函数
+ function confirm_delete(comment_id ) {
+    // 调用layer弹窗组件
+    layer.open({
+        // 弹窗标题
+        title: "delete comment？",
+        // 正文
+        content: "This can’t be undone and it will be removed from the web. ",
+        // 点击按钮后调用的回调函数
+        yes: function(index, layero) {
+            // 指定应当前往的 url
+            
+            delete_comment(comment_id)
+        },
+    })
+}
 
 function like_category(category_id) {
     console.log(category_id);
