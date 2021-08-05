721c0a128a97930e39be58b8ca2fe677026b118d
Yue Zhao <2499059z@student.gla.ac.uk>
2021-08-05 07:54:16 +0100

    update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,33 +2,33 @@ function delete_comment(comment_id) {
     console.log(comment_id);
     $.ajax({
         url: '/rango/delete_comment/',
-        type:"POST",
-        data: {"comment_id":comment_id},
-        success: function(e){
-        if(e=="1"){
-            parent.location.reload();
-            console.log("删除成功");
-        }else{
-            console.log("删除失败");
-        }
+        type: "POST",
+        data: { "comment_id": comment_id },
+        success: function (e) {
+            if (e == "1") {
+                parent.location.reload();
+                console.log("delete successed");
+            } else {
+                console.log("delete failed");
+            }
 
         },
     })
 
 
 }
- 
- function confirm_delete(comment_id ) {
-    
+
+function confirm_delete(comment_id) {
+
     layer.open({
         // title
         title: "delete comment？",
         // text
         content: "This can’t be undone and it will be removed from the web. ",
         // click the button and call the function
-        yes: function(index, layero) {
+        yes: function (index, layero) {
             // go to the url or the function
-            
+
             delete_comment(comment_id)
         },
     })
@@ -38,15 +38,15 @@ function like_category(category_id) {
     console.log(category_id);
     $.ajax({
         url: '/rango/like_category/',
-        type:"POST",
-        data: {"category_id":category_id},
-        success: function(e){
-        if(e=="1"){
-            parent.location.reload();
-            console.log("点赞成功");
-        }else{
-            console.log("点赞失败");
-        }
+        type: "POST",
+        data: { "category_id": category_id },
+        success: function (e) {
+            if (e == "1") {
+                parent.location.reload();
+                console.log("like successed");
+            } else {
+                console.log("like failed");
+            }
 
         },
     })
@@ -57,15 +57,15 @@ function like_comment(comment_id) {
     console.log(comment_id);
     $.ajax({
         url: '/rango/like_comment/',
-        type:"POST",
-        data: {"comment_id":comment_id},
-        success: function(e){
-        if(e=="1"){
-            parent.location.reload();
-            console.log("点赞成功");
-        }else{
-            console.log("点赞失败");
-        }
+        type: "POST",
+        data: { "comment_id": comment_id },
+        success: function (e) {
+            if (e == "1") {
+                parent.location.reload();
+                console.log("like successed");
+            } else {
+                console.log("like failed");
+            }
 
         },
     })
@@ -73,14 +73,4 @@ function like_comment(comment_id) {
 }
 
 
-
-
-$(function () {
-
-    
-    console.log('okokok');
-
-    
-
-});
 
